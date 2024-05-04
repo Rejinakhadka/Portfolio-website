ebcf47f7dc1cd8d6d478eb5c64852e3e0f960778
rejina <rejinakhadka00@gmail.com>
2024-05-04 17:01:58 +0545

    feat(Ui-myworks-contact)

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,8 +33,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        "service_8w56ipd",
+        "template_wo1nd0o",
         {
           from_name: form.name,
           to_name: "Rejina Khadka",
@@ -42,7 +42,7 @@ const Contact = () => {
           to_email: "rejinakhadka00@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        "6_Xjda3crMOBDQXhN",
       )
       .then(
         () => {
--- src/constants/index.js
+++ src/constants/index.js
@@ -19,10 +19,10 @@ import {
   starbucks,
   tesla,
   shopify,
-  carrent,
   jobit,
   tripguide,
   threejs,
+  movix,
 } from "../assets";
 
 export const navLinks = [
@@ -157,67 +157,45 @@ const testimonials = [
 
 const projects = [
   {
-    name: "Car Rent",
+    name: "Movix",
     description:
-      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      "Web-based platform that allows users to browse varours trending movies and watch trailer.",
     tags: [
       {
         name: "react",
         color: "blue-text-gradient",
       },
       {
-        name: "mongodb",
+        name: "reduxjs/toolkit",
         color: "green-text-gradient",
       },
       {
-        name: "tailwind",
+        name: "sass",
         color: "pink-text-gradient",
       },
     ],
-    image: carrent,
-    source_code_link: "https://github.com/",
+    image: movix,
+    source_code_link: "https://github.com/Rejinakhadka/movixreactwebapp",
   },
   {
     name: "Job IT",
-    description:
-      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    description: "A simple Form Management System.",
     tags: [
       {
         name: "react",
         color: "blue-text-gradient",
       },
       {
-        name: "restapi",
+        name: "local storage",
         color: "green-text-gradient",
       },
       {
-        name: "scss",
+        name: "tailwind css",
         color: "pink-text-gradient",
       },
     ],
     image: jobit,
-    source_code_link: "https://github.com/",
-  },
-  {
-    name: "Trip Guide",
-    description:
-      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
-    tags: [
-      {
-        name: "nextjs",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "supabase",
-        color: "green-text-gradient",
-      },
-      {
-        name: "css",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: tripguide,
-    source_code_link: "https://github.com/",
+    source_code_link: "https://github.com/Rejinakhadka/crud_reactjs",
   },
 ];
 
